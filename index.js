88de803e014db5f7a0c49b637f87bf198f8c4b5a
namanns <namanns@sbgtv.com>
2026-03-24 17:35:25 -0700

    Add health check route

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.json({ status: 'ok' });
+});
+
 app.get ('/api/weather', async (req, res) => {
     const { city } = req.query;
 
