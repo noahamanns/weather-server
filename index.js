c263c34077ec39762a44417505264e1643932f2e
namanns <namanns@sbgtv.com>
2026-03-24 17:25:14 -0700

    Debug API key loading

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config();
 
+console.log('API_KEY loaded:', process.env.API_KEY ? 'yes' : 'no');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
